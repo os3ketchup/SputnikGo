feecd4b1ffd4ba7c0b3d7f7cbe033b9b996e7c30
Eldor Teshaboev <eldorteshaboyev1994@gmail.com>
2022-10-25 09:56:10 +0500

    Data layer: Adding from Domain layer

---
+++ app/src/main/java/uz/os3ketchup/sputnikgo/presentation/LoginActivity.kt
@@ -0,0 +1,12 @@
+package uz.os3ketchup.sputnikgo.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.os3ketchup.sputnikgo.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
